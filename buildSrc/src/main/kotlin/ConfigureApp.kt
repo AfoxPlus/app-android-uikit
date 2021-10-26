7d633e0504ef760a3e6f5995c8a4eb1b1542899c
Valentin Mendoza Torres <jmendozat13@gmail.com>
2021-10-26 16:51:56 -0500

    Se modifico el ConfigureApp y el sonarqube (#6)
    Co-authored-by: Carlos Osorio <hacybeyker@gmail.com>

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -1,7 +1,8 @@
 object ConfigureApp {
     const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/app-yalisto-android"
     const val organization = "afoxplus"
-    const val applicationId = "app-android-uikit"
+    const val projectName = "app-android-uikit"
+    const val applicationId = "app.android.uikit"
     const val groupId = "com.afoxplus.android"
     const val artifactId = "uikit"
     const val version = "1.1.0"
