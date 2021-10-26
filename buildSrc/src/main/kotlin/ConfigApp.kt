95a6c9c85602123d36f3b4cd19ab8b4bb97ef818
Valentin Mendoza <jmendozat13@gmail.com>
2021-10-26 00:37:58 -0500

    Update configapp

--- buildSrc/src/main/kotlin/ConfigApp.kt
+++ buildSrc/src/main/kotlin/ConfigApp.kt
@@ -3,5 +3,5 @@ object ConfigApp {
     const val applicationId = "app-android-uikit"
     const val groupId = "com.afoxplus.android"
     const val artifactId = "uikit"
-    const val version = "1.0.4"
+    const val version = "1.1.0"
 }
