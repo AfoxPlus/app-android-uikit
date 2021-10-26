e643cf4589febd3f31e41a182f58086b38a3e463
Valentin Mendoza <jmendozat13@gmail.com>
2021-10-26 00:33:52 -0500

    Added new config

--- buildSrc/src/main/kotlin/ConfigApp.kt
+++ buildSrc/src/main/kotlin/ConfigApp.kt
@@ -1,5 +1,5 @@
 object ConfigApp {
-    const val urlRepoDependencies = "https://maven.pkg.github.com/AfoxPlus/app-android-uikit"
+    const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/app-yalisto-android"
     const val applicationId = "app-android-uikit"
     const val groupId = "com.afoxplus.android"
     const val artifactId = "uikit"
