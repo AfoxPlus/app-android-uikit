fe89ca80c048cc601be1d0e2ec4035ca56c60e8f
Valentin Mendoza <jmendozat13@gmail.com>
2021-11-19 22:53:35 -0500

    Remove package network

--- module/src/main/java/com/afoxplus/uikit/service/exceptions/ApiNetworkException.kt
+++
@@ -1,8 +0,0 @@
-package com.afoxplus.uikit.service.exceptions
-
-import java.io.IOException
-
-class ApiNetworkException(
-    val code: Int,
-    override val message: String = ""
-) : IOException(message)
--- module/src/main/java/com/afoxplus/uikit/service/exceptions/NetworkException.kt
+++
@@ -1,5 +0,0 @@
-package com.afoxplus.uikit.service.exceptions
-
-import java.io.IOException
-
-class NetworkException : IOException("To use this app you need an internet connection")
