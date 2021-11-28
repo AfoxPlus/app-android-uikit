b3d024836675683cd28c8dfcd0c0fd0c1f5b2430
Petter Jhunior Garcia Infante <petterjunior94@gmail.com>
2021-11-28 10:54:40 -0500

    add icons for orders

---
+++ module/src/main/java/com/afoxplus/uikit/customview/ButtonAction.kt
@@ -0,0 +1,10 @@
+package com.afoxplus.uikit.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ButtonAction(context: Context, attrs: AttributeSet, defStyleAttr: Int = 0) :
+    @JvmOverloads ConstraintLayout(context, attrs, defStyleAttr) {
+        
+}
