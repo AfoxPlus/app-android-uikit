cbd3514b52005ce472c60961274a2ec36022c4da
Petter Jhunior Garcia Infante <petterjunior94@gmail.com>
2022-04-04 20:03:42 -0500

    fix quantity button issue

--- module/src/main/java/com/afoxplus/uikit/customview/quantitybutton/ButtonAction.kt
+++ module/src/main/java/com/afoxplus/uikit/customview/quantitybutton/ButtonAction.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 
-class ButtonAction(context: Context, attrs: AttributeSet, defStyleAttr: Int = 0) :
-    @JvmOverloads ConstraintLayout(context, attrs, defStyleAttr) {
+class ButtonAction(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
 
 }
