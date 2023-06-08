7a699340c8e97e06e90fa0a854c4cf23b2f5b561
Valentin Mendoza <jmendozat13@gmail.com>
2023-06-07 21:45:15 -0500

    Update parameter

--- module/src/main/java/com/afoxplus/uikit/objects/vendor/Vendor.kt
+++ module/src/main/java/com/afoxplus/uikit/objects/vendor/Vendor.kt
@@ -3,5 +3,5 @@ package com.afoxplus.uikit.objects.vendor
 data class Vendor(
     val tableId: String,
     val restaurantId: String,
-    val additionalInfo: Map<String, Any> = emptyMap()
+    val additionalInfo: MutableMap<String, Any> = mutableMapOf()
 )
