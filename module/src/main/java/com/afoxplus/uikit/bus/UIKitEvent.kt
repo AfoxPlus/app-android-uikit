7399b70caae0fb6df014bd63f3189db30da0fc18
Valentin Mendoza <jmendozat13@gmail.com>
2023-08-03 00:31:20 -0500

    Refactor event Bus

--- module/src/main/java/com/afoxplus/uikit/bus/UIKitEvent.kt
+++ module/src/main/java/com/afoxplus/uikit/bus/UIKitEvent.kt
@@ -2,6 +2,7 @@ package com.afoxplus.uikit.bus
 
 import androidx.lifecycle.Observer
 
+@Deprecated("Use SharedFlow for emit events in yours viewmodels")
 open class UIKitEvent<out T>(private val content: T) {
     var hasBeenHandled = false
         private set
@@ -18,6 +19,7 @@ open class UIKitEvent<out T>(private val content: T) {
     fun peekContent(): T = content
 }
 
+@Deprecated("Use SharedFlow in yours viewmodels")
 class UIKitEventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<UIKitEvent<T>> {
     override fun onChanged(UIKitEvent: UIKitEvent<T>?) {
         UIKitEvent?.getContentIfNotHandled()?.let {
