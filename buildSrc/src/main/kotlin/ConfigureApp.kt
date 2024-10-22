52a9ec5222f7c1bbcd3a18726255441b396a7e26
Petter Infante <petter.infante@macys.com>
2024-10-22 16:10:29 -0500

    update the version to 6.2.7

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -2,7 +2,7 @@ object ConfigureApp {
     const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/app-yalisto-android"
     const val groupId = "com.afoxplus.android"
     const val artifactId = "uikit"
-    const val versionName = "6.2.6"
+    const val versionName = "6.2.7"
     const val organization = "afoxplus"
     const val projectName = "app-android-${artifactId}"
 }
