e3824e96841eda19062ef4bfd4e821780e7b677e
Carlos Osorio <hacybeyker@gmail.com>
2024-11-01 17:25:44 -0500

    fix uikitbutton
    Took 2 hours 26 minutes

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -2,7 +2,7 @@ object ConfigureApp {
     const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/app-yalisto-android"
     const val groupId = "com.afoxplus.android"
     const val artifactId = "uikit"
-    const val versionName = "6.2.8"
+    const val versionName = "6.2.9"
     const val organization = "afoxplus"
     const val projectName = "app-android-${artifactId}"
 }
