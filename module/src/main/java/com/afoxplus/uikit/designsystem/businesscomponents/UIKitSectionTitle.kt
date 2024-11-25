7925fec75cd4f23942234bbdd25a085381be87ff
Valentin Mendoza <jmendozat13@gmail.com>
2024-11-25 17:19:19 -0500

    Remove preview method

--- module/src/main/java/com/afoxplus/uikit/designsystem/businesscomponents/UIKitSectionTitle.kt
+++ module/src/main/java/com/afoxplus/uikit/designsystem/businesscomponents/UIKitSectionTitle.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import com.afoxplus.uikit.designsystem.atoms.UIKitIcon
 import com.afoxplus.uikit.designsystem.atoms.UIKitText
 import com.afoxplus.uikit.designsystem.atoms.UIKitVerticalDashedLine
@@ -77,17 +76,4 @@ fun UIKitSectionTitle(
         }
     }
 
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewSectionTitle() {
-    UIKitTheme {
-        UIKitSectionTitle(
-            modifier = Modifier.fillMaxWidth(),
-            title = "Establecimiento"
-        )
-
-    }
 }
